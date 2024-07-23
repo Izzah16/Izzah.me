edbe99f1261bcb9452211e2f1c03502ae63442a9
unknown <izzahbhutta@gmail.com>
2024-07-23 23:02:38 +0500

    Update text in [filename]

--- src/screen/Testimonial.jsx
+++ src/screen/Testimonial.jsx
@@ -13,13 +13,11 @@ export const Testimonial = () => {
           <div className="testimonials-widget grid5">
             {testimonialData.map((testimonial) => (
               <div className="testimonial-item" key={testimonial.id}>
-                <div className="top-area flex">
-                  <div className="logo-box">
-                    <img src={testimonial.logo} alt="" />
-                  </div>
+                
+                 
                   <div className="image-box">
                     <img src={testimonial.userImage} alt="" />
-                  </div>
+                 
                 </div>
                 <div className="icon-box">
                   <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
